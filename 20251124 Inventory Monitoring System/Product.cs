0372da222b2f4568eb115c1ed8a589183854f719
chrismgbhs <116180951+chrismgbhs@users.noreply.github.com>
2025-12-01 11:04:05 +0800

    Add Type property and update Product constructor

--- 20251124 Inventory Monitoring System/Product.cs
+++ 20251124 Inventory Monitoring System/Product.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +9,16 @@ namespace _20251124_Inventory_Monitoring_System
     internal class Product
     {
         public string Name;
+        public string Type;
 
         /// <summary>
         /// This constructor initializes a new instance of the Product class with the specified name.
         /// </summary>
         /// <param name="name"></param>
-        public Product(string name)
+        public Product(string name, string type)
         {
             Name = name;
+            Type = type;
         }
     }
 }
